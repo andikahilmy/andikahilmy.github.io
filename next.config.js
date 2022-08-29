082fb0aa5e1b399c5deb13ecef0a2aef601953b6
dawetmaster <andika.naufal48@gmail.com>
2022-08-29 14:03:12 +0700

    perbaiki config nextjs

--- next.config.js
+++ next.config.js
@@ -2,9 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images: {
-    loader: 'akamai',
-    path: '',
+  experimental: {
+    images: {
+      unoptimized: true,
+    },
   },
 }
 
