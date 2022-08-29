65a9ba7d2ddccdcb3715bfa8897c7627c95a6076
dawetmaster <71202342+dawetmaster@users.noreply.github.com>
2022-08-29 07:04:24 +0000

    Update Github Page

---
+++ _next/static/MnT4DXY4NZYnzj8bSFo15/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ae0e3e027412e072.css","static/chunks/pages/index-9b20d877a1a3d23b.js"],"/_error":["static/chunks/pages/_error-66eb88f2a856484a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
--- _next/static/k5J8ANC2bDhCA66iFcB70/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/763f6ea39a450fea.css","static/chunks/pages/index-741ba3ca1e9d1268.js"],"/_error":["static/chunks/pages/_error-66eb88f2a856484a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
