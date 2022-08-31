eb017f314d4090c6539e493acee2654cf04327bb
dawetmaster <andika.naufal48@gmail.com>
2022-08-31 19:11:38 +0700

    resolve module hilang

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Splash from '../components/splash/splash'
-import styles from '../styles/Home.module.css'
 const bgImage = 'bg.jpg'
 
 export default function Home() {
