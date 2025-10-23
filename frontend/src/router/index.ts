bdca53e287890ad81bb4ea85e30607f474da5e88
andikahilmy <andika.naufal48@gmail.com>
2025-10-23 10:45:51 +0700

    CHORE: restart from scratch

---
+++ frontend/src/router/index.ts
@@ -0,0 +1,19 @@
+// import { createRouter, createWebHashHistory } from 'vue-router'
+// import Home from '../pages/Home.vue'
+// import About from '../pages/About.vue'
+// import Portfolio from '../pages/Portfolio.vue'
+// import Blog from '../pages/Blog.vue'
+// import CV from '../pages/CV.vue'
+
+// const routes = [
+//   { path: '/', component: Home },
+//   { path: '/about', component: About },
+//   { path: '/portfolio', component: Portfolio },
+//   { path: '/blog', component: Blog },
+//   { path: '/cv', component: CV },
+// ]
+
+// export const router = createRouter({
+//   history: createWebHashHistory(),
+//   routes,
+// })
