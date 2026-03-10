5464ad795d29ae4b60fdcaaab0c962af3dde735e
andikahilmy <andika.naufal48@gmail.com>
2026-03-10 23:07:23 +0700

    persiapan transisi ke Kanagawa

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,11 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'andikahilmy';
+export const SITE_DESCRIPTION = 'Welcome to my digital sanctuary!';
+
+// Socials and links
+export const LINKEDIN_LINK = 'https://linkedin.com/in/andikahilmy'
+export const GITHUB_LINK = 'https://github.com/andikahilmy'
+export const FACEBOOK_LINK = 'https://facebook.com/andhilmy'
+export const INSTAGRAM_LINK = 'https://facebook.com/andhilmy'
